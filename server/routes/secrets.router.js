5dd0822568fbbd11bc293f612cc72ee0f998cf07
Brad Loudis <Bradley.Loudis@gmail.com>
2020-11-20 16:57:28 -0600

    narrowing down the proper query text for secrets.router

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,14 +1,46 @@
 const express = require('express');
 const pool = require('../modules/pool');
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
   console.log('req.user:', req.user);
+  const queryUserRole = `SELECT * FROM "user" WHERE id=$1`;
+  // GET route code here
+
+  // Sarah's query idea...
+  // SELECT *
+  // FROM "user"
+  // JOIN "secret" ON "user".clearance_level > "secret".secrecy_level
+  // WHERE "user".clearance_level >= "secret".secrecy_level
+  // ORDER BY username ASC;
+
   pool
-    .query('SELECT * FROM "secret";')
-    .then((results) => res.send(results.rows))
-    .catch((error) => {
-      console.log('Error making SELECT for secrets:', error);
+    .query(queryUserRole, [req.user.id])
+    .then((response) => {
+      console.log('response', response);
+      const clearanceLevel = response.rows[0].clearance_level;
+
+      const queryText = `SELECT *
+      FROM "secret"
+      WHERE "secret".secrecy_level < $1
+      ORDER BY id ASC;`;
+
+      pool
+        .query(queryText, [clearanceLevel])
+        .then((responseSecret) => {
+          console.log('responseSecret', responseSecret);
+          res.send(responseSecret.rows);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.sendStatus(500);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
       res.sendStatus(500);
     });
 });
